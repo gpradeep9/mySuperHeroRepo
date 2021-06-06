f4c06887a32704869e2207e4d21c0d64aed6375c
pradeep gupta <gpradeep906@gmail.com>
2021-06-06 21:10:53 +0530

    added steve to dev branch

--- src/superheroRepo/Application.java
+++ src/superheroRepo/Application.java
@@ -5,6 +5,9 @@ public class Application {
 	public static void main(String[] args) {
 		System.out.println("Hi I am main class of superheros");
 		Hulk hulk= new Hulk();
+		
+		System.out.println("Hi I am Dev branch");
+		Steve steve = new Steve();
 
 	}
 
---
+++ src/superheroRepo/Steve.java
@@ -0,0 +1,10 @@
+package superheroRepo;
+
+public class Steve {
+
+	public Steve() {
+		System.out.println("i can do this all day");
+		System.out.println(" i am 75 years old");
+	}
+
+}
