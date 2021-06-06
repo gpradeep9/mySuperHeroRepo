76b896e8936d444279db75d8db17cd8b65d2ff5a
pradeep gupta <gpradeep906@gmail.com>
2021-06-06 21:07:44 +0530

    my secong commit added hulk to list

--- src/superheroRepo/Application.java
+++ src/superheroRepo/Application.java
@@ -4,6 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hi I am main class of superheros");
+		Hulk hulk= new Hulk();
 
 	}
 
---
+++ src/superheroRepo/Hulk.java
@@ -0,0 +1,11 @@
+package superheroRepo;
+
+public class Hulk {
+
+	public Hulk() {
+		System.out.println("Hi i m superstrong");
+		System.out.println(" i have 5 phd");
+
+	}
+
+}
